51f47fb227407f4addeb0bb4e41bffb5341bdedd
joao pedro <joaopdrost@gmail.com>
2022-03-18 18:46:54 -0300

    atividade

---
+++ src/server.ts
@@ -0,0 +1,66 @@
+const prompt = require("prompt-sync")();
+
+	//let x = prompt("Informe um número: ");
+	//let y = prompt("Informe um número: ");
+	//let num1 = Number(x);
+	//let num2 = Number(y);
+
+	//let soma = num1+num2;
+	//console.log(`Soma: ${soma}`);
+//     let somatotal = 0
+//     for(let i = 1; i < 2; i++ ){
+//         let x = prompt( `informe ${i} numero:`);
+//         somatotal = somatotal + Number(x); 
+//     }
+//     console.log(`Soma Total: ${somatotal}`);
+//    if(somatotal > 0){
+//     console.log("Esse numero é positivo")
+// }else if(somatotal < 0){
+//     console.log("Esse numero é negativo")
+//    }else{
+//     console.log("Esse numero é nulo")
+//    }
+   
+// let x
+// while (x!=0) {
+//   x = prompt('informe um numero:');
+//    console.log(`rais quadrada `,Number(x)*Number(x))
+// }
+// let x = 5 
+// do{
+//     console.log(`rais quadrada `,Number(x)*Number(x))
+//     x--
+// }while(x!=0);
+//let x = prompt("Informe um número: ");
+
+// switch(Number(x)){
+//     case 1:
+//         console.log ("voce é aluno");
+//         break;
+//      case 2:
+//             console.log ("voce é Professor");
+//          break;
+//     case 3:
+//         console.log ("voce é coordenador");
+//         break;
+// }
+
+// let fruits1: string[] = ["Apple","Orange", "Banana"];
+
+// for (let i = 0; i < fruits1.length; i++) {
+// 	console.log(fruits1[i])   
+// }
+let sabores
+for (let i = 0; i < sabores.length; i++) {
+	sabores[i] = prompt(`Informe o ${i+1} sabor:`)
+	
+}
+sabores.push("laranja")
+console.log
+	
+
+    
+
+      
+
+
